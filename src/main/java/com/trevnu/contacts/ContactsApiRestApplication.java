b6404dab8745b15668a60a60562892dbdf88d1cb
Humberto M <humbertomonterrosa@gmail.com>
2023-03-03 22:56:46 -0500

    first commit

---
+++ src/main/java/com/trevnu/contacts/ContactsApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.trevnu.contacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trevnu/contacts/ContactsApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trevnu.contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
